8f87f279b25e725eeed6b81d490b9e69e3652cb4
ihsakashi <yuvch122@gmail.com>
2020-08-26 20:00:00 -0600

    update for aospa

--- neos.pick.sh
+++ neos.pick.sh
@@ -16,15 +16,15 @@ safer-git() {
 }
 
 pushd frameworks/base
-safer-git remote add 2neos ${REMOTE_URL}/android_frameworks_base.git
-safer-git fetch 2neos neos-picks
-safer-git cherry-pick 957cf93c70 # packages: settingslib: development settings enabled default 
-safer-git cherry-pick b50aff69ac # PackageManager: Don't delete unknown apps 
+safer-git remote add neos ${REMOTE_URL}/android_frameworks_base.git
+safer-git fetch neos quartz
+safer-git cherry-pick 7a3b71aea4 # packages: settingslib: development settings enabled default 
+safer-git cherry-pick a9e285ee09 # PackageManager: Don't delete unknown apps 
 popd
 
 pushd system/core
-safer-git remote add 2neos ${REMOTE_URL}/android_system_core.git
-safer-git fetch 2neos neos-picks
-safer-git cherry-pick 7fe424d02f # Boot on charger state too
-safer-git cherry-pick a01bdc2a5b # add comma permissions to fs_config
+safer-git remote add neos ${REMOTE_URL}/android_system_core.git
+safer-git fetch neos quartz
+safer-git cherry-pick 40cac9f8b7 # Boot on charger state too
+safer-git cherry-pick 40cac9f8b7 # add comma permissions to fs_config
 popd
